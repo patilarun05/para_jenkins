1dd81241aeb63b4866e9a143aab49c26e3cc6509
patilarun05 <patilarun05@gmail.com>
2023-11-08 02:32:02 +0530

    code refactor

---
+++ src/main/java/com/example/para_jenkins/ParaJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.para_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/para_jenkins/ParaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.para_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
